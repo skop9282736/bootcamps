71dc52961765be214e83e68b226646c46a9f1eef
ismailaidar <ismail.aidar@yahoo.com>
2019-11-03 16:00:58 -0600

    - Get Single course

--- controller/course.controller.js
+++ controller/course.controller.js
@@ -8,21 +8,37 @@ const Bootcamp = require('./../models/Bootcamp');
 // @route     GET /api/v1/bootcamps/:bootcampId/courses
 // @access    Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-    let query;
-    
-    if(req.params.bootcampId) {
-      query = Course.find({bootcamp: req.params.bootcampId}).populate({
-        path: 'bootcamp',
-        select: 'name description'
-      });
-    } else {
-      query = Course.find().populate('bootcamp');
-    }
+  let query;
 
-    const courses = await query;
-    res.status(200).send({
-      success: true,
-      count: courses.length,
-      data: courses
-    })
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId }).populate({
+      path: 'bootcamp',
+      select: 'name description'
+    });
+  } else {
+    query = Course.find().populate('bootcamp');
+  }
+
+  const courses = await query;
+  res.status(200).send({
+    success: true,
+    count: courses.length,
+    data: courses
+  });
 });
+
+// @desc      Get courses
+// @route     GET /api/v1/courses/:id
+// @access    Public
+exports.getCourse = async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate('bootcamp');
+
+  if (!course) {
+    return new ErrorResponse('No Course with this if', 404);
+  }
+
+  res.status(200).send({
+    success: true,
+    data: course
+  });
+};
--- routes/courses.route.js
+++ routes/courses.route.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const {
-  getCourses,
-} = require('./../controller/course.controller');
+const { getCourses, getCourse } = require('./../controller/course.controller');
 
 const router = express.Router({ mergeParams: true });
 
-router
-  .route('/')
-  .get(getCourses)
+router.route('/').get(getCourses);
+
+router.route('/:id').get(getCourse);
 
 module.exports = router;
